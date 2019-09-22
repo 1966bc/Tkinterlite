76cc81ae43356ba0c2d0bf992369f61ccd5eb795
1966bc <giuseppecostanzi@gmail.com>
2019-09-22 06:11:29 +0200

    Add files via upload

--- tkinterlite.py
+++ tkinterlite.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 import profile
 import pstats
 import frames.main as main
 
-main.main()
-#to try performance.....
-#profile.run('main.main()', 'profile_results')
-#p = pstats.Stats('profile_results')
-#p.sort_stats('cumulative').print_stats(20)
+if len(sys.argv)>1:
+    profile.run('main.main()', 'profile_results')
+    p = pstats.Stats('profile_results')
+    p.sort_stats('cumulative').print_stats(10)
+else:
+    main.main()
 
 
 
