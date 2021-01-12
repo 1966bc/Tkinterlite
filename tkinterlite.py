7460f880bfd56a01afd6c25614ec40307e7fda19
1966bc <giuseppecostanzi@gmail.com>
2021-01-12 06:39:22 +0100

    Update tkinterlite.py

--- tkinterlite.py
+++ tkinterlite.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#!/usr/bin/python3
 #-----------------------------------------------------------------------------
 # project:  tkinterlite
 # authors:  1966bc
